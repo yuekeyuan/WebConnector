71e4fe7878480f3f5e1d360664357822c4cbb79e
yuekeyuan <yuekeyuan@qq.com>
2015-07-21 22:38:49 +0800

    version2
    submit

--- com/yue/gui/MainWindow.py
+++ com/yue/gui/MainWindow.py
@@ -16,7 +16,7 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.setupUi(self)
 
     def initEvent(self):
-        self.ui.closeButton.clicked.connect(self.close)
+        self.ui.closeButton.clicked.connect(QtGui.qApp.quit)
         self.ui.propertyButton.clicked.connect(self.openAndSetPropertyWindow)
         self.ui.contentFileBrowser.clicked.connect(self.getFilePath)
 
@@ -29,7 +29,8 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.postButton.clicked.connect(self.postEvent)
         self.ui.putButton.clicked.connect(self.putEvent)
         self.ui.deleteButton.clicked.connect(self.deleteEvent)
-
+        self.ui.paramentersTableView.clicked.connect(self.paramClick)
+        self.ui.headersTabelView.clicked.connect(self.headersClick)
 
     def openAndSetPropertyWindow(self):
         print("open properties windows")
@@ -38,17 +39,86 @@ class MainWindow(QtGui.QMainWindow):
     def getFilePath(self):
         print("get file path")
 
+    def paramClick(self, index):
+        row = index.row()
+        print(row)
+        key = self.ui.paramentersTabelModel.item(row, 0).text()
+        value = self.ui.paramentersTabelModel.item(row, 1).text()
+        self.ui.paramKeyEidt.setText(key)
+        self.ui.paramValueEidt.setText(value)
+
     def paramAdd(self):
-        pass
+        key = self.ui.paramKeyEidt.text()
+        value = self.ui.paramValueEidt.text()
+        if key == "" or key == None:
+            QtGui.QMessageBox.information(None, None, "请输入键", "确定")
+            return
+        if value == "" or value == None:
+            QtGui.QMessageBox.information(None, None, "请输入值", "确定")
+            return
+
+        rows = self.ui.paramentersTabelModel.findItems(key, QtCore.Qt.MatchExactly, 0)
+        if rows == None or len(rows) == 0:
+            print("not find")
+            row = []
+            row.append(QtGui.QStandardItem(key))
+            row.append(QtGui.QStandardItem(value))
+            self.ui.paramentersTabelModel.appendRow(row)
+            print("append success")
+        else:
+            #逐个去找
+            for i in range(self.ui.paramentersTabelModel.rowCount()):
+                if self.ui.paramentersTabelModel.item(i, 0).text() == key:
+                    self.ui.paramentersTabelModel.setItem(i, 1, QtGui.QStandardItem(value))
 
     def paramRemove(self):
-        pass
+        key = self.ui.paramKeyEidt.text()
+        self.ui.paramKeyEidt.setText("")
+        self.ui.paramValueEidt.setText("")
+        for i in range(self.ui.paramentersTabelModel.rowCount()):
+                if self.ui.paramentersTabelModel.item(i, 0).text() == key:
+                    self.ui.paramentersTabelModel.removeRow(i)
+                    return
+
+    def headersClick(self, index):
+        row = index.row()
+        print(row)
+        key = self.ui.headersTabelModel.item(row, 0).text()
+        value = self.ui.headersTabelModel.item(row, 1).text()
+        self.ui.headersKeyEdit.setText(key)
+        self.ui.headersValueEdit.setText(value)
 
     def headersAdd(self):
-        pass
+        key = self.ui.headersKeyEdit.text()
+        value = self.ui.headersValueEdit.text()
+        if key == "" or key == None:
+            QtGui.QMessageBox.information(None, None, "请输入键", "确定")
+            return
+        if value == "" or value == None:
+            QtGui.QMessageBox.information(None, None, "请输入值", "确定")
+            return
+
+        rows = self.ui.headersTabelModel.findItems(key, QtCore.Qt.MatchExactly, 0)
+        if rows == None or len(rows) == 0:
+            print("not find")
+            row = []
+            row.append(QtGui.QStandardItem(key))
+            row.append(QtGui.QStandardItem(value))
+            self.ui.headersTabelModel.appendRow(row)
+        else:
+            #逐个去找
+            for i in range(self.ui.headersTabelModel.rowCount()):
+                if self.ui.headersTabelModel.item(i, 0).text() == key:
+                    self.ui.headersTabelModel.setItem(i, 1, QtGui.QStandardItem(value))
 
     def headersRemove(self):
-        pass
+        key = self.ui.headersKeyEdit.text()
+        self.ui.headersKeyEdit.setText("")
+        self.ui.headersValueEdit.setText("")
+        for i in range(self.ui.headersTabelModel.rowCount()):
+                if self.ui.headersTabelModel.item(i, 0).text() == key:
+                    self.ui.headersTabelModel.removeRow(i)
+                    return
 
     def getEvent(self):
         print("run get event")
@@ -64,8 +134,6 @@ class MainWindow(QtGui.QMainWindow):
         result.setModal(False)
         result.show()
 
-
-
     def postEvent(self):
         print("run post event")
 
@@ -80,3 +148,7 @@ class MainWindow(QtGui.QMainWindow):
 
     def getParamentersValue(self):
         return None
+
+    #################################################################
+    #  below is window event and others
+    #################################################################
